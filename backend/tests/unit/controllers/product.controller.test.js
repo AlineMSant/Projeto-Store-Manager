10b3a4a5c455d64b2ac933de3b1c102bead32216
AlineMSant <alinems4120@gmail.com>
2023-05-26 17:26:55 -0300

    requisito 1 concluido

---
+++ backend/tests/unit/controllers/product.controller.test.js
@@ -0,0 +1,63 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+const { expect } = chai;
+chai.use(sinonChai);
+
+const { productService } = require('../../../src/services');
+const { productController } = require('../../../src/controllers');
+const { allProducts } = require('./mock/product.controller.mock');
+
+describe('Teste de unidade do productController', function () {
+  describe('Listando todas os produtos', function () {
+    it('Deve retornar o status 200 e todos os produtos', async function () {
+      const res = {};
+      const req = {};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon.stub(productService, 'findAll').resolves({ type: null, message: allProducts });
+
+      await productController.getAllProducts(req, res);
+
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(allProducts);
+    });
+  });
+
+  describe('Listando um produto', function () {
+    it('Deve retornar o status 200 e o produto referente ao id', async function () {
+      const res = {};
+      const req = { params: { id: 1 } };
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon.stub(productService, 'findById').resolves({ type: null, message: allProducts[0] });
+
+      await productController.getProductById(req, res);
+
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(allProducts[0]);
+    });
+
+    it('Deve retornar o status 404 e mensagem de erro caso não exista ', async function () {
+      const res = {};
+      const req = { params: { id: 999 } };
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon.stub(productService, 'findById')
+      .resolves({ type: 'PRODUCT_NOT_FOUND', message: 'Product not found' });
+
+      await productController.getProductById(req, res);
+
+      expect(res.status).to.have.been.calledWith(404);
+      expect(res.json).to.have.been.calledWith({ message: 'Product not found' });
+    });
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+});
